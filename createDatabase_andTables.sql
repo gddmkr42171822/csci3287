70d5912ec88124e80af1c0eec840f15eb679d35e
Bob <bob@Bobs-MacBook-Pro.local>
2014-11-11 23:20:17 -0700

    first table and one set of values

--- createDatabase_andTables.sql
+++ createDatabase_andTables.sql
@@ -1,2 +1,20 @@
 CREATE DATABASE db_project;
 USE db_project;
+
+CREATE TABLE countries
+(
+  name varchar(255)
+  language varchar(255)
+  nationality varchar(255)
+  population bigint
+  continent varchar(255)
+  capitol varchar(255)
+  PRIMARY KEY (name)
+  #FOREIGN KEY ()
+);
+
+#TFD: name -> language, language does not -> name, language -> nationality
+#MVD: continent ->> language, continent ->> name;
+
+INSERT INTO countries (name, language, nationality, population, continent, capitol);
+VALUES ('germany', 'german', 'german', 80620000, 'europe', 'berlin');
