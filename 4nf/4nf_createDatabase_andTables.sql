4bfb042d95a5d2fd9f080c42f78fa725045fb6d6
Bob <bob@reshut1-48-17-dhcp.int.colorado.edu>
2014-12-07 22:57:06 -0700

    fixed some erros in the tfd and talbes

--- 4nf/4nf_createDatabase_andTables.sql
+++ 4nf/4nf_createDatabase_andTables.sql
@@ -209,7 +209,7 @@ CREATE TABLE flag_colors
   country varchar(255),
   date_flag_created varchar(255),
   flag_colors varchar(255),
-  PRIMARY KEY(flag_colors),
+  PRIMARY KEY(cause_of_flag_creation, flag_colors, country),
   FOREIGN KEY(country) REFERENCES countries(name),
   FOREIGN KEY(cause_of_flag_creation) REFERENCES flag_day(cause_of_flag_creation)
 );
@@ -221,7 +221,7 @@ CREATE TABLE flag_sizes
   country varchar(255),
   date_flag_created varchar(255),
   flag_sizes varchar(255),
-  PRIMARY KEY(flag_sizes),
+  PRIMARY KEY(cause_of_flag_creation, flag_size, country),
   FOREIGN KEY(country) REFERENCES countries(name),
   FOREIGN KEY(cause_of_flag_creation) REFERENCES flag_day(cause_of_flag_creation)
 );
